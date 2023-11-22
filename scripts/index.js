696003c82cf9c616f3349f5883f94b8eded3e78e
Fritz Gerald Alexandre <fritzgeraldalexandre7@gmail.com>
2023-11-22 00:49:27 -0500

    added location map

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// JavaScript
+document.querySelectorAll("*").forEach((element) => {
+  element.classList.add("fade-in");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  });
+
+  observer.observe(element);
+});
