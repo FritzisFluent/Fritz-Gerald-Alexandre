74b445a376f33cc8005d8b4f4e2754aaa2215c6e
Fritz Gerald Alexandre <fritzgeraldalexandre7@gmail.com>
2023-11-25 23:46:16 -0500

    Added sticky positon to the hamburger menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+// Select the hamburger button
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  navMenu.classList.toggle("show");
+  if (navMenu.classList.contains("show")) {
+    hamburger.innerHTML = '<i class="fas fa-times"></i>'; // X icon
+  } else {
+    hamburger.innerHTML = '<i class="fas fa-bars"></i>'; // Hamburger icon
+  }
+});
+
 // Scroll down effect for the skills section.
 document.addEventListener("DOMContentLoaded", (event) => {
   const skillsSection = document.querySelector(".skills"); // Select the skills section
